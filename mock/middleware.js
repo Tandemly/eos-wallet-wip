08d499c9e9d6edefa3dcd97345bb31381248faf4
Orlando La <ceanela@users.noreply.github.com>
2017-10-20 16:31:46 -0500

    Working toward functional mock server

--- mock/middleware.js
+++ mock/middleware.js
@@ -1,6 +1,20 @@
-// hello.js
+const nJwt = require('njwt');
+const secureRandom = require('secure-random');
+
 module.exports = (req, res, next) => {
+  const { body } = req;
+
+  console.log(body);
+
+  const claims = {
+    iss: "http://localhost:4400/",
+    ...body,
+  };
+
+  const signingKey = secureRandom(256, { type: 'Buffer' });
+  const jwt = nJwt.create(claims, signingKey);
+
   // res.header('X-Hello', 'World')
-  console.log('hello!');
+  console.log('hello!', jwt);
   next()
 }
