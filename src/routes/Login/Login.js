c3eaa773ffaad505f244303dab1a54ced8488419
Michael Kimberlin <mkimberlin@gmail.com>
2017-11-17 11:40:50 -0600

    less than elegant modal is true/false fix

--- src/routes/Login/Login.js
+++ src/routes/Login/Login.js
@@ -6,13 +6,10 @@ const Login = ({ handleClose, test }) => (
     <div className="login-header modal-header">
       <h3 className="title is-3">Login to your wallet</h3>
 
-      <button
-        className="js-modal-close"
-        onClick={handleClose}
-      />
+      <button className="js-modal-close" onClick={handleClose} />
     </div>
     <div className="modal-body">
-      <Container />
+      <Container modal />
     </div>
   </div>
 );
