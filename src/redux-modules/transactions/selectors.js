70b13a2c32470733c886af6515d0f602c7cabc4a
Michael Kimberlin <mkimberlin@gmail.com>
2017-11-17 11:11:59 -0600

    fix a missing definition

--- src/redux-modules/transactions/selectors.js
+++ src/redux-modules/transactions/selectors.js
@@ -1,12 +1,12 @@
 import { createSelector } from "reselect";
-import { selectEOSAccountName } from '../eos-account/selectors';
+import { selectEOSAccountName } from "../eos-account/selectors";
 
 const selectTransactionsState = state => state.transactions;
 
 export const selectRecentTransactions = createSelector(
   selectTransactionsState,
   selectEOSAccountName,
-  transactions =>
+  (transactions, eosAccountName) =>
     transactions.recent &&
     transactions.recent
       .filter(item => item && item.scope && item.scope.includes(eosAccountName))
